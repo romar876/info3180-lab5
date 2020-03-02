a0d1b4be4db3b3d803e4dd57931874fac8b0d1c8
Romar Plummer <rplummer@atlassian.com>
2020-03-01 21:22:55 -0500

    working

--- app/views.py
+++ app/views.py
@@ -10,8 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
-
-
+from werkzeug.security import check_password_hash
 ###
 # Routing for your application.
 ###
@@ -34,20 +33,19 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
+            username = form.username.data
+            password = form.password.data
             # Get the username and password values from the form.
+            
+            user = UserProfile.query.filter_by(username=username).first()
 
-            # using your model, query database for a user based on the username
-            # and password submitted. Remember you need to compare the password hash.
-            # You will need to import the appropriate function to do so.
-            # Then store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method below.
-
-            # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            if user is not None and check_password_hash(user.password, password ):
+                login_user(user)
+                flash("Login Successful", "Successful")
+                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
+            else:
+                flash("Unsuccessful Login", "Unsuccesful")
     return render_template("login.html", form=form)
 
 
@@ -60,6 +58,19 @@ def load_user(id):
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
+@app.route('/secure_page')
+@login_required
+def secure_page():
+    """Render a secure page on our website that only logged in users can access."""
+    return render_template('secure_page.html')
+    
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'danger')
+    return redirect(url_for('home'))
 
 
 @app.route('/<file_name>.txt')
