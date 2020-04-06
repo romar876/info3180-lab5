047749d652de5e737bc0e12ff9af7f37b6f8f53a
Romar Plummer <rplummer@atlassian.com>
2020-04-06 00:06:58 -0500

    working

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:mantis101@localhost/lab5"
+app.config['SECRET_KEY'] = "this is a super secure key"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:1234@localhost/lab5"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
@@ -16,3 +16,4 @@ login_manager.login_view = 'login'
 
 app.config.from_object(__name__)
 from app import views
+
